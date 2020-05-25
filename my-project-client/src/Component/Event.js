ccaa3a55d3fb71afd4c2a936b054aad1a059b6ee
Deijah Wilson <deijahwilson1@gmail.com>
2020-05-25 15:35:16 -0400

    modal working and close functionality working

--- my-project-client/src/Component/Event.js
+++ my-project-client/src/Component/Event.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Button, Header, Image, Modal, ModalDescription } from 'semantic-ui-react'
 
 const Event=(props)=>{
 
@@ -8,17 +8,52 @@ const Event=(props)=>{
     })
 
     return(
+        
+        
         <div>
-            <h2>{props.event._def.title}</h2> 
-            <p>Hosted by: {group.name}</p>
-            {/* <img src={props.event._def.extendedProps.address} /> */}
-            <p>{props.event._instance.range.end.toDateString()}</p>
-            <p>{props.event._def.extendedProps.address}</p>
-            <p>{props.event._def.extendedProps.city}, {props.event._def.extendedProps.state}</p>
-            <h4>{props.event._def.extendedProps.description}</h4>
+       
+        <Modal.Header> <h1>{props.event._def.title}</h1> </Modal.Header>
+            <Modal.Content image> 
+                <Image wrapped size='medium' src={props.event._def.extendedProps.image} />
+               
+                <Modal.Description>
+                <Header><p>Hosted by: {group.name}</p></Header>
+                    <p>{props.event._instance.range.end.toDateString()}</p>
+                    <p>{props.event._def.extendedProps.address}</p>
+                    <p>{props.event._def.extendedProps.city}, {props.event._def.extendedProps.state}</p>
+                    <h4>{props.event._def.extendedProps.description}</h4>
+                </Modal.Description>
+            </Modal.Content>
+
+            <Modal.Actions>
+            <Button negative onClick={(event) => props.handleCloseButton(event)}>Close</Button>
+            <Button
+              positive
+              icon='checkmark'
+              labelPosition='right'
+              content='RSVP'
+            />
+          </Modal.Actions>
+       
         </div>
+    
     )
 
 }
+
+{/* <Modal trigger={<Button>Show Modal</Button>}> */}
+                    {/* <Modal.Header>Select a Photo</Modal.Header>
+                    <Modal.Content image>
+                    <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
+                    <Modal.Description>
+                        <Header>Default Profile Image</Header>
+                        <p>
+                        We've found the following gravatar image associated with your e-mail
+                        address.
+                        </p>
+                        <p>Is it okay to use this photo?</p>
+                    </Modal.Description>
+                    </Modal.Content> */}
+                {/* </Modal> */}
 
 export default Event
--- my-project-client/src/Container/EventsContainer.js
+++ my-project-client/src/Container/EventsContainer.js
@@ -3,14 +3,16 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction';
 import Event from '../Component/Event.js'
-import { Button, Popup } from 'semantic-ui-react'
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
+// import { Button, Popup } from 'semantic-ui-react'
 
 class EventsContainer extends Component{
     
     constructor(){
         super()
         this.state={
-            event: null
+            event: null,
+            showEvent: false, 
         }
     } 
 
@@ -18,9 +20,19 @@ class EventsContainer extends Component{
         
         this.setState({
             // showEvent : !this.state.showEvent,
-            event : e.event
+            event : e.event,
+            showEvent: !this.state.showEvent
         })
     }
+    handleCloseButton=(event)=>{
+       event.preventDefault();
+        this.closeModal()
+    }
+    closeModal = () => {
+        this.setState({
+            showEvent : false
+        })
+      }
 
 
     render(){
@@ -35,11 +47,31 @@ class EventsContainer extends Component{
                     // make the event dates dynamic
                   eventsArr  
                 }/>
-                {this.state.event ? 
-                <Event event={this.state.event} groups={this.props.groups}/>
+
+                
+                {this.state.event ?    
+                <Modal open={this.state.showEvent} centered={true}>  
+                <Event event={this.state.event} groups={this.props.groups} handleCloseButton={this.handleCloseButton}/>
+                </Modal> 
                 :
                 false
                 }
+
+                {/* <Modal trigger={<Button>Show Modal</Button>}> */}
+                    {/* <Modal.Header>Select a Photo</Modal.Header>
+                    <Modal.Content image>
+                    <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
+                    <Modal.Description>
+                        <Header>Default Profile Image</Header>
+                        <p>
+                        We've found the following gravatar image associated with your e-mail
+                        address.
+                        </p>
+                        <p>Is it okay to use this photo?</p>
+                    </Modal.Description>
+                    </Modal.Content> */}
+                {/* </Modal> */}
+
             </div>
         ) 
     }
